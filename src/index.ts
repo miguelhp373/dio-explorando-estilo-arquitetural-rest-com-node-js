142f754c315a7568f787c00a590e373f1ef64d47
Miguel Henrique <miguelhp373@gmail.com>
2023-12-29 03:07:44 -0300

    init  - server express on application

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-"use strict";
+import express, {Request, Response, NextFunction} from "express";
 
-console.log('teste');
+const app = express();
+
+app.get('/status',(req: Request, res: Response, next: NextFunction)=>{
+    res.status(200).send({foo: 'bar'});
+});
+
+app.listen(3000, () => console.log('Server Running on 3000!'));
